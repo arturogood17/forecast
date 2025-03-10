c64a84bc543d37ce0efe88adeaf9e5868a95636c
arturogood17 <arturo_good17@hotmail.com>
2025-03-10 07:23:17 +0100

    Added cache to client

--- internal/weatherapi/Client.go
+++ internal/weatherapi/Client.go
@@ -3,14 +3,18 @@ package weatherapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/arturogood17/forecast/internal/weathercache"
 )
 
 type Client struct {
+	Cache      weathercache.Cache
 	httpClient *http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, interval time.Duration) Client {
 	c := Client{
+		Cache: weathercache.WCache(interval),
 		httpClient: &http.Client{
 			Timeout: timeout,
 		},
